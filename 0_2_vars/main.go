86f4c174ec863f72fdd1c4e8be05420bb48ca3d5
B0209123 <B0209123@hydn4634.solco.global.nttdata.com>
2019-05-27 12:38:31 +0530

    add run other files

--- 0_2_vars/main.go
+++ 0_2_vars/main.go
@@ -3,8 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var b = 1
+	// Note
+
+	/*
+		int is an alias of Int32, so, int and Int32 are the same type. Int32 represents 32-bits (4-bytes) signed integer. Int32 occupies 32-bits (4-bytes) space in the memory. As per the 4-bytes data capacity, an Int32's value capacity is -2147483648 to +2147483647.
+
+	*/
+	var b int32 = 1
 	var c = "Manish"
-	fmt.Printf("B _______%T", b)
-	fmt.Printf("C_______%T", c)
+	//ShortHand
+	// name := "Manish"
+	// email := "Manish@gmail.com"
+	//Shorthand
+	name, email := "Manish", "Manish@gmail.com"
+	fmt.Printf("B _______%T\n", b)
+	fmt.Printf("C_______%T\n", c)
+	fmt.Println(name, email, b, c)
+
 }
---
+++ 0_3_packages/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/manishjha1991/go_crash_course/0_3_packages/strutil"
+)
+
+func main() {
+
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(math.Ceil(2.7))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println(strutil.Reverse("Manish"))
+}
